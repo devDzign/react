9ef1e77381a7e517d887c8d2ad33869c89e68946
mchabour <mchabour@largus.fr>
2017-03-31 12:26:21 +0200

    component react

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-// Ici on code notre App
+import React from 'react';
+import { render } from 'react-dom';
 
+
+class App extends React.Component{
+
+    render(){
+        return (
+            <h1>Coucou </h1>
+        )
+    }
+}
+
+
+render(
+    <App />,
+     document.getElementById('root')
+);
